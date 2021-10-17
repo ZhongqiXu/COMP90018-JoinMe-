20675bd6238be16fa19d4da519f4d50f16c7c0ea
DexterZhu0702 <70119740+DexterZhu0702@users.noreply.github.com>
2021-10-17 21:33:11 +0800

    ban edit

--- JoinMe/app/src/main/java/com/comp90018/JoinMe/MyActivityDetailActivity.java
+++ JoinMe/app/src/main/java/com/comp90018/JoinMe/MyActivityDetailActivity.java
@@ -91,7 +91,7 @@ public class MyActivityDetailActivity extends AppCompatActivity implements Navig
                 startActivity(new Intent(MyActivityDetailActivity.this, MyActivityListActivity.class));
             }
         });
-
+        /*
         edit = findViewById(R.id.edit);
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,6 +101,8 @@ public class MyActivityDetailActivity extends AppCompatActivity implements Navig
                 startActivity(intent);
             }
         });
+
+         */
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,11 +113,13 @@ public class MyActivityDetailActivity extends AppCompatActivity implements Navig
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
-            case R.id.edit:
+            /*case R.id.edit:
                 Intent intent = new Intent(MyActivityDetailActivity.this, EditActivityActivity.class);
                 intent.putExtra("activityInfo", activityInfo);
                 startActivity(intent);
                 break;
+
+             */
             case R.id.delete:
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
                 alertDialog
