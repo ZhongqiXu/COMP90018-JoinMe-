01598d060026c39b9234bae3e89342cb1d2d1c74
SIQI <zsqseraphina@outlook.com>
2021-10-31 23:18:14 +0800

    add me page

--- JoinMe/app/src/main/java/com/comp90018/JoinMe/MeActivity.java
+++ JoinMe/app/src/main/java/com/comp90018/JoinMe/MeActivity.java
@@ -8,21 +8,83 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import object.User;
 
 public class MeActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
     NavigationBarView bottomNavigationView;
     private Button profile,my,join,logout;
+
+    TextView welname;
+    FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    private ImageView profileImageview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
 
+        mAuth = FirebaseAuth.getInstance();
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(this);
         bottomNavigationView.getMenu().findItem(R.id.Me).setChecked(true);
+
+        final String uid = mAuth.getUid();
+        welname = findViewById(R.id.wel_name);
+        profileImageview=findViewById(R.id.profileImageview);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("user").child(uid);
+
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = firebaseFirestore.collection("User").document(uid);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                //System.out.println(user);
+                welname.setText(user.getUserName());
+
+                documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                String uri = (String) document.getString("image");
+                                Picasso.get().load(uri).into(profileImageview);
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         profile = findViewById(R.id.profile);
         profile.setOnClickListener(new View.OnClickListener() {
